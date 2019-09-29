376c048078bc34f5e5e4a7750858eb91b9f9e556
Joe Ferguson <joe@joeferguson.me>
2019-09-28 21:43:47 -0500

    Add deployment info

---
+++ Envoy.blade.php
@@ -0,0 +1,8 @@
+@servers(['web' => ['joeferguson@server.joeferguson.me']])
+
+@task('deploy', ['on' => 'web'])
+cd /home/joeferguson/domains/vcdt.joeferguson.me/vcdt
+git pull origin master
+composer install --no-dev
+php artisan migrate --force
+@endtask
