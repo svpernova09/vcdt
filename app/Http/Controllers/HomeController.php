dd0c32e5cc403b207283f05bdbc003903435e42d
Joe Ferguson <joe@joeferguson.me>
2019-10-04 22:44:28 -0500

    Updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use VCDT\Services\ProcessBoxes;
 
 class HomeController extends Controller
 {
     public function home():array
     {
-        $run = new ProcessBoxes();
-        $run();
+        $now = Carbon::now();
+        $yesterday = Carbon::now('UTC')->subDay();
+        $max = DB::table('downloads')
+                 ->whereBetween('created_at', [$yesterday, $now])
+                 ->max('downloads');
+        $min = DB::table('downloads')
+                 ->whereBetween('created_at', [$yesterday, $now])
+                 ->min('downloads');
 
-        return [
+        $result = [
             'status' => 'ok',
             'msg' => 'nothing to see here',
+            'last_24_hours' => $max - $min,
         ];
+
+        return $result;
     }
 }
