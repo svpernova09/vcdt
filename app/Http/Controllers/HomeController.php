3e424259e32072f4757ad2b0c3e6e8d5090235ef
Joe Ferguson <joe@joeferguson.me>
2019-12-05 19:52:09 -0600

    Add new state + intro caching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use VCDT\Services\ProcessBoxes;
 
@@ -11,21 +12,37 @@ class HomeController extends Controller
 {
     public function home():array
     {
+        $minutes = 5;
         $now = Carbon::now();
         $yesterday = Carbon::now('UTC')->subDay();
-        $max = DB::table('downloads')
-                 ->whereBetween('created_at', [$yesterday, $now])
-                 ->max('downloads');
-        $min = DB::table('downloads')
-                 ->whereBetween('created_at', [$yesterday, $now])
-                 ->min('downloads');
 
-        $result = [
+        $max = Cache::remember('max', $minutes, function () use ($now, $yesterday) {
+            return DB::table('downloads')
+                     ->whereBetween('created_at', [$yesterday, $now])
+                     ->max('downloads');
+        });
+
+        $min = Cache::remember('min', $minutes, function () use ($now, $yesterday) {
+            return DB::table('downloads')
+                     ->whereBetween('created_at', [$yesterday, $now])
+                     ->min('downloads');
+        });
+
+        $first = Cache::remember('first', $minutes, function () {
+            return DB::table('downloads')->where('id', '=', 1)->first();
+        });
+
+        $last = Cache::remember('last', $minutes, function () {
+            return DB::table('downloads')->where('id', \DB::table('downloads')->max('id'))->first();
+        });
+
+        $downloads = $last->downloads - $first->downloads;
+//        dd($downloads);
+        return [
             'status' => 'ok',
             'msg' => 'nothing to see here',
             'last_24_hours' => $max - $min,
+            'since_'.str_replace(' ', '_', $first->created_at) => $downloads,
         ];
-
-        return $result;
     }
 }
