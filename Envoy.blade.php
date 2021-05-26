1a4b54bc8fe9ee17efe443f6b15f2aa2ec961f74
Joe Ferguson <joe@joeferguson.me>
2021-05-26 11:52:16 -0500

    update deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['web' => ['joeferguson@server.joeferguson.me']])
+@servers(['web' => ['joeferguson@jameson.joeferguson.me']])
 
 @task('deploy', ['on' => 'web'])
 cd /home/joeferguson/domains/vcdt.joeferguson.me/vcdt
